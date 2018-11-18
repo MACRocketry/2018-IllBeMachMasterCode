7ad236ffdf4ceb3df958ba49230e716254a553c0
matt-dalessandro <36138251+matt-dalessandro@users.noreply.github.com>
2018-11-18 16:16:51 -0500

    Update masterCode.ino

--- masterCode.ino
+++ masterCode.ino
@@ -1,3 +1,4 @@
+//Import libraries here
 #include <SPI.h> //native library for serial peripheral interfacing (sd shield)
 #include <SD.h> //native library for SD reading nd writing
 #include <MacRocketry_GPS_Shield.h>  //Jerrys library for the GPS shield
@@ -7,8 +8,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 
-
-MacRocketry_SD_Logger logger;
+//Initialzie objects here
+MacRocketry_SD_Logger logger;  
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(12345);
 MacRocketry_GPS_Shield gps;
 LED_Diagnostics led;
@@ -20,8 +21,41 @@ void setup() {
   initializeSensors();
   initializeSD();
   
+  state = 1;
+  stateMachine(state);
+}
+
+void stateMachine(int state) {
   rocketInfo = [float array]; //static memory array initialized
-  diagnostics(rocketInfo);
+  while (True) {
+    if (state == 1) {  //diagnostics stage
+      rocketInfo = diagnostics(rocketInfo);
+      if (condition for next state) {
+        state++;
+      }
+    }
+    
+    else if (state == 2) {  //ascentToDrogue stage
+      rocketInfo = ascentToDrogue(rocketInfo);
+      if (condition for next state) {
+        state++;
+      }
+    }
+    
+    else if (state == 3) {  //descentToMain
+      rocketInfo = descentToMain(rocketInfo);
+      if (condition for next state) {
+        state++;
+      }
+    }
+    
+    else if (state == 2) {  //descentToGround
+      rocketInfo = descentToGround(rocketInfo);
+      if (condition for next state) {
+        state++;
+      }
+    }
+  }
 }
 
 float diagnostics(float rocketInfo) {  //this phase will operate before we launch the rocket
@@ -32,12 +66,7 @@ float diagnostics(float rocketInfo) {  //this phase will operate before we launc
   
   led.statusCheck(*Check to see if we can pass already taken data*);
   
-  if (conditionToNextPhase) {
-    ascentToDrogue(rocketInfo);
-  }
-  else {
-    diagnostics(rocketInfo);
-  }
+  return rocketInfo;
 }
 
 float ascentToDrogue(float rocketInfo) {  //this phase will run up to and including laucnhing the drogue chute
@@ -46,13 +75,7 @@ float ascentToDrogue(float rocketInfo) {  //this phase will run up to and includ
   rocketInfo = dataAnalytics(gpsData, bmpData);  //returns array containing p, p', v, v', a, a', along with any GPS data we want
   storeData(rocketInfo);
   
-  if (conditionToNextPhase) {
-    deployDrogue();
-    descentToMain(rocketInfo);
-  }
-  else {
-    ascentToDrogue(rocketInfo);
-  }
+  return rocketInfo;
 }
 
 void descentToMain(float rocketInfo) {  //this phase will operate starting after the deployment of the drogue up to and including the deployment of the main chute
@@ -61,20 +84,16 @@ void descentToMain(float rocketInfo) {  //this phase will operate starting after
   rocketInfo = dataAnalytics(gpsData, bmpData);  //returns array containing p, p', v, v', a, a', along with any GPS data we want
   storeData(rocketInfo);
   
-  if (conditionToNextPhase) {
-    deployMain();
-    descent(rocketInfo);
-  }
-  else {
-    descentToMain(rocketInfo);
-  }
+  return rocketInfo;
 }
 
-void descent(float rocketInfo) {  //this phase ill run after the chutes have been deployed and until the rocket lands
+void descentToGround(float rocketInfo) {  //this phase ill run after the chutes have been deployed and until the rocket lands
   gpsData = readGPS();  //use Jerrys library
   bmpData = readBMP();  //use adafruit library
   rocketInfo = dataAnalytics(gpsData, bmpData);  //returns array containing p, p', v, v', a, a', along with any GPS data we want
   storeData(rocketInfo);
+  
+  return rocketInfo;
 }
 
 bool initializeSD() {
